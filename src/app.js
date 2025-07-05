9cb329e8474446b26238e0beaed4cea93879d7c5
Ajay Chauhan <ajaychauhanpp44@gmail.com>
2025-07-05 13:33:22 +0530

    findout params for daynaimc routing

--- src/app.js
+++ src/app.js
@@ -5,21 +5,9 @@ const express = require('express');
  const port = 7777;
 
   app.get("/user" , (req , res) => {
+     console.log(req.query)
       res.send({firstName : "Ajay" , lastName: "Chauhan"})
   })
-  app.post("/user" , (req , res) => {
-    //   console.log(`Save Data into a Database`)
-    // Saving data into database
-      res.send(`Data successfully saved to the database`)
-  })
-  app.delete("/user" , (req , res) => {
-    
-      res.send(`Data successfully Deleted form database`)
-  })
-  app.use("/test" , (req , res) => {
-      res.send('This is Test page');
-  })
-
  app.listen(port, () => {
       console.log(`Serveris running port ${port}`)
  })
