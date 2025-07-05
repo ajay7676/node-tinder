e5005b73325a916e0e0f99bb61a41ad838c3494d
Ajay Chauhan <ajaychauhanpp44@gmail.com>
2025-07-05 12:48:46 +0530

    Create basic api for testing http method like : GET , POST , DELETE

--- src/app.js
+++ src/app.js
@@ -3,15 +3,23 @@ const express = require('express');
  const app = express();
    
  const port = 7777;
-  app.get("/" , (req , res) => {
-      res.send('This is main page')
+
+  app.get("/user" , (req , res) => {
+      res.send({firstName : "Ajay" , lastName: "Chauhan"})
   })
-  app.get("/home" , (req , res) => {
-      res.send('This is home page')
+  app.post("/user" , (req , res) => {
+    //   console.log(`Save Data into a Database`)
+    // Saving data into database
+      res.send(`Data successfully saved to the database`)
   })
-  app.get("/profile" , (req , res) => {
-      res.send('This is profile page')
+  app.delete("/user" , (req , res) => {
+    
+      res.send(`Data successfully Deleted form database`)
   })
+  app.use("/test" , (req , res) => {
+      res.send('This is Test page');
+  })
+
  app.listen(port, () => {
       console.log(`Serveris running port ${port}`)
  })
