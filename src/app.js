d73723bd81c073aa56157b614d4038155fbaa8c4
Ajay Chauhan <ajaychauhanpp44@gmail.com>
2025-07-06 16:03:34 +0530

    Error Handling

--- src/app.js
+++ src/app.js
@@ -1,27 +1,28 @@
 const express = require('express');
-const { adminAuth , userAuth } = require('./middleware/auth');
 
  const app = express();
    
  const port  = 7777;
 
-  app.use("/admin" , adminAuth);
+   
+   app.use("/" , (err ,req,res , next ) =>{
+           console.log("Error routing is working")
+     if(err){
+          res.status(500).send("something went wrong")
+     }
 
-//   app.use("/user" , userAuth);
-   app.post("/user/login" , (req,res) => {
-      res.send("User Logged is successful")
    })
-  app.get("/user" ,userAuth , (req,res) => {
-      console.log("User Controller is  running...")
-      res.send("User Data sent !!!!")
 
-  })
- app.get("/admin/getAllData" , (req,res) => {
-        res.send("All Data Sent");
- })
- app.get("/admin/deleteUser" , (req,res) => {
-      res.send("Deleated User");
- })
+ app.get("/getUserData" , (req,res) => {
+      try {
+          throw new Error("sdsdsdsd");
+      res.send("User Data sent")
+      } catch (error) {
+            res.status(500).send("Some Error contact support team")
+      }
+       
+
+   });
 
  app.listen(port, () => {
       console.log(`Serveris running port ${port}`)
