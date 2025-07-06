070c612e980573e5ad5990ef75a65aa0f4f8a977
Ajay Chauhan <ajaychauhanpp44@gmail.com>
2025-07-06 10:58:51 +0530

    Play With multiple handler

--- src/app.js
+++ src/app.js
@@ -4,10 +4,31 @@ const express = require('express');
    
  const port = 7777;
 
-  app.get("/user" , (req , res) => {
-     console.log(req.query)
-      res.send({firstName : "Ajay" , lastName: "Chauhan"})
-  })
+  app.use("/user" , (req, res ,next) =>{
+     // Route Handler
+      console.log(`Handling the route user`);
+      next();
+     //  res.send("Response !!");
+  }, 
+  (req, res , next) =>{
+     // Route Handler
+      console.log(`Handling the route user 2`)
+     //  res.send("2 Response !!")
+     next();
+  },
+   (req, res , next) =>{
+     // Route Handler
+      console.log(`Handling the route user 3`)
+     //  res.send("3 Response !!")
+     next();
+  },
+   (req, res , next) =>{
+     // Route Handler
+      console.log(`Handling the route user 4`)
+      res.send("4 Response !!")
+  }
+
+)
  app.listen(port, () => {
       console.log(`Serveris running port ${port}`)
  })
