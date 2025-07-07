ad2325664aac04cf73cc7635f7663c1015fc907a
Ajay Chauhan <ajaychauhanpp44@gmail.com>
2025-07-07 14:15:58 +0530

    Add Mongo DB Compass with cloude mongodb clustors

---
+++ src/database.js
@@ -0,0 +1,10 @@
+// Go to mongodb website 
+// Create a free M0 cluster
+// Create a user
+// Get the connection String
+
+// const URI = "mongodb+srv://ajaychauhanpp44:<ph9OmQ1nTuP7mcoN>@namaste-node.5r8ejyg.mongodb.net/"
+// mongodb+srv://ajaychauhanpp44:<ph9OmQ1nTuP7mcoN>@namaste-node.5r8ejyg.mongodb.net/
+
+
+// Install Mongodb Compass into system
