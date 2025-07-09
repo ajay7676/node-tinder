a1456dde87f36b774ccb63f308431d20d0650b85
Ajay Chauhan <ajaychauhanpp44@gmail.com>
2025-07-10 00:58:29 +0530

    Configure Model into app.js to insert data in database

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const connectDB = require("./config/database");
+const UserModel = require("./model/user")
 
 const app = express();
 
 const port = 7777;
+
+// app.use(express.json());
+
+app.post("/signup" , async(req , res) => {
+      // Creating  a new instance of the User Model
+        const user = new UserModel({
+          firstName : "Rahul" , 
+          lastName: "Chauhan",
+          emailId: "rahul@gmail.com",
+          password : "rahul@3$"
+        })
+         console.log("Signup is rendering")
+        await user.save();
+        res.send("User Added Successfuly")
+})
 
 connectDB()
   .then(() => {
