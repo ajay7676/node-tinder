b563d20d3dcb65e7752180664110f35b76c7e7f8
Ajay Chauhan <ajaychauhanpp44@gmail.com>
2025-07-12 12:38:12 +0530

    Made User API to get user & users data

--- src/app.js
+++ src/app.js
@@ -11,8 +11,8 @@ app.use(express.json());
 app.post("/signup" , async(req , res) => {
   //  console.log(req.body)
       // Creating  a new instance of the User Model
-        const user = new UserModel(req.body)
-
+        const user = new UserModel(req.body);
+         console.log(user)
         try {
            await user.save();
            res.send("User Added Successfuly")
@@ -22,6 +22,44 @@ app.post("/signup" , async(req , res) => {
         }
        
 })
+ // Get user by email
+  app.get("/user" , async(req,res) =>{
+     const userEmail = req.body.emailId
+    try{ 
+        
+        //  const users = await UserModel.find({ emailId : userEmail});
+        //  if(users.length === 0){
+        //    res.status(404).send("User is not found")
+        //  }
+        //  else{
+        //     res.send(users        
+
+        //     )
+        //  }
+
+        // if we neede to find one user then we will use UserModel.fineOne()
+         const user = await UserModel.findOne({})
+         try {
+            if(!user){
+              res.status(404).send("User is not found")
+            }else{
+              res.send(user)
+            }
+          
+         } catch (error) {
+          
+         }
+        } catch (error) {
+          res.status(404).send("Something went wrong " , + error.message)
+          
+        }
+
+
+  })
+
+// Feed API GET/feed get all the users from the database
+
+app.get("/feed" , )
 
 connectDB()
   .then(() => {
